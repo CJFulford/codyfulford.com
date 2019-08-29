42195a1e14575299da2648adbcea3567aa1bdaac
Cody Fulford <cody@codyfulford.com>
2019-08-29 12:32:21 -0600

    Navbar brand is no longer a link.

--- navbar.php
+++ navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-black mb-3">
-    <a class="navbar-brand" href="#">Cody Fulford</a>
+    <div class="navbar-brand">
+        Cody Fulford
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
