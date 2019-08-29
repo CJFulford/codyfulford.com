05402b860a30b34f611bb188b3dc75202e813f6c
Cody Fulford <cody@codyfulford.com>
2019-08-29 12:45:17 -0600

    Removed global-tab-icon since there is already a favicon.

--- global-header.php
+++ global-header.php
@@ -2,8 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="author" content="Cody Fulford">
 
-<link rel="icon" href="media/global-tab-icon.png">
-
 <!--Bootstrap CSS-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
