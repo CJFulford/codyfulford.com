50deb199362d2c05d70ea7653c6328b1af9f8159
Cody Fulford <cody@codyfulford.com>
2019-08-29 12:42:32 -0600

    Homepage has been formatted.

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include 'global-header.php'; ?>
-        <title>Cody Fulford</title>
-    </head>
-    <body>
-        <?php include "navbar.php"; ?>
-        <div class="container">
-            <div class="row text-justify">
-                <div class="col">
-                    <p>My name is Cody Fulford.</p>
-                    <p>I currently live in Calgary, Alberta, Canada where I was born in 1994.</p>
 
-                    <p>I am currently employed at Integrity Post Structures in Okotoks, Alberta, Canada where I work under Daniel Maher building an industry leading job management program.</p>
+<head>
+    <?php include 'global-header.php'; ?>
+    <title>Cody Fulford</title>
+</head>
 
-                    <p>In April of 2017, I finished my five-year career at the University of Calgary, graduating with a bachelors degree in computer science, graphics concentration.</p>
-                </div>
+<body>
+    <?php include "navbar.php"; ?>
 
-                <div class="col-12">
-                    <h4 class="border-bottom border-red mt-2">Academic Career</h4>
-                    <p>When my university studies began I was majoring in archaeology. However, I soon realized that I loved history, but found the act of archaeology far too dry to continue professionally. I branched out, trying to find a new path and eventually enrolled in an introductory programming class. I quickly realized just how much computers can do in a short period of time and became very excited with the possibilities. I changed my major to computer science and eventually added a concentration for computer graphics. After being accepted to the graphics concentration, I felt that I had found my purpose and poured everything I had into my schooling. Dramatically increasing my marks and truly enjoying every one of the classes that I took. In 2017, I graduated university with a degree and a GPA of 3.9 In my final year and 3.6 overall.</p>
+    <div class="container">
+        <div class="row text-justify">
+            <div class="col">
+                <p>My name is Cody Fulford.</p>
+                <p>I currently live in Calgary, Alberta, Canada where I was born in 1994.</p>
 
-                    <h4 class="border-bottom border-red mt-2">Athletic Career</h4>
-                    <p>My passion for sports started at Dr. E.P. Scarlett High School in Calgary, Alberta, Canada. The first time I entered the high school I was immediately asked to join the football team by one of the coaches. I found my love of football and of sports in general during the following season. After a relatively successful season, I was one of two recipients out of a team of fifty that received the Lancer Pride Award (see the quote at the top of the page). I strive to give the same level of effort and commitment to every aspect of my life. After graduating high school, I returned to the Scarlet football field as a coach where I became the offensive line coach for 3 years before becoming the offensive coordinator in my fourth year. I have since taken a break from coaching to focus my efforts on my university studies and programming career. I have been skiing since I was two years old and golfing since fourteen. I am a fan of the Calgary Stampeders, Flames, and Seattle Seahawks </p>
-                </div>
+                <p>I am currently employed at Integrity Post Structures in Okotoks, Alberta, Canada where I work under Daniel Maher building an industry leading job management program.</p>
+
+                <p>In April of 2017, I finished my five-year career at the University of Calgary, graduating with a bachelors degree in computer science, graphics concentration.</p>
+            </div>
+
+            <div class="col-12">
+                <h4 class="border-bottom border-red mt-2">Academic Career</h4>
+                <p>When my university studies began I was majoring in archaeology. However, I soon realized that I loved history, but found the act of archaeology far too dry to continue professionally. I branched out, trying to find a new path and eventually enrolled in an introductory programming class. I quickly realized just how much computers can do in a short period of time and became very excited with the possibilities. I changed my major to computer science and eventually added a concentration for computer graphics. After being accepted to the graphics concentration, I felt that I had found my purpose and poured everything I had into my schooling. Dramatically increasing my marks and truly enjoying every one of the classes that I took. In 2017, I graduated university with a degree and a GPA of 3.9 In my final year and 3.6 overall.</p>
+
+                <h4 class="border-bottom border-red mt-2">Athletic Career</h4>
+                <p>My passion for sports started at Dr. E.P. Scarlett High School in Calgary, Alberta, Canada. The first time I entered the high school I was immediately asked to join the football team by one of the coaches. I found my love of football and of sports in general during the following season. After a relatively successful season, I was one of two recipients out of a team of fifty that received the Lancer Pride Award (see the quote at the top of the page). I strive to give the same level of effort and commitment to every aspect of my life. After graduating high school, I returned to the Scarlet football field as a coach where I became the offensive line coach for 3 years before becoming the offensive coordinator in my fourth year. I have since taken a break from coaching to focus my efforts on my university studies and programming career. I have been skiing since I was two years old and golfing since fourteen. I am a fan of the Calgary Stampeders, Flames, and Seattle Seahawks </p>
             </div>
         </div>
-        <?php include 'global-footer.php'; ?>
-        <script>$(document).ready(function () { $('#navbar-index').addClass('active rounded'); });</script>
-    </body>
+    </div>
+    <?php include 'global-footer.php'; ?>
+    <script>
+        $(document).ready(function() {
+            $('#navbar-index').addClass('active rounded');
+        });
+    </script>
+</body>
+
 </html>
