6f2307272ac61726e5f0f6b2ca3285c9db438c39
Cody Fulford <cody@codyfulford.com>
2020-01-28 18:13:24 -0700

    All files have now been updated with the intelephence formatter.

--- renderings.php
+++ renderings.php
@@ -1,39 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include 'global-header.html'; ?>
-        <title>Cody Fulford - Portfolio</title>
-    </head>
-    <body>
-        <?php include "navbar.html"; ?>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-12 border-bottom border-red mb-2"><h4>Asteroid Belt</h4></div>
-                <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/asteroid_belt_2-low_res.png"></div>
-            </div>
+<head>
+    <?php include 'global-header.html'; ?>
+    <title>Cody Fulford - Portfolio</title>
+</head>
 
-            <div class="row">
-                <div class="col-12 border-bottom border-red mb-2"><h4>Rocket Man</h4></div>
-                <div class="col-12 text-center"><iframe class="border border-dark mw-100" src="https://www.youtube.com/embed/fWEc1RXHcfo" frameborder="0" allowfullscreen></iframe></div>
-            </div>
+<body>
+    <?php include "navbar.html"; ?>
 
-            <div class="row">
-                <div class="col-12 border-bottom border-red mb-2"><h4>Fish Bowl</h4></div>
-                <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/fish_bowl.png"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 border-bottom border-red mb-2">
+                <h4>Asteroid Belt</h4>
             </div>
-
-            <div class="row">
-                <div class="col-12 border-bottom border-red mb-2"><h4>For The Love Of Toast</h4></div>
-                <div class="col-12 text-center"><iframe class="border border-dark mw-100" src="https://www.youtube.com/embed/cn0Y8NESzEM" frameborder="0" allowfullscreen></iframe></div>
-            </div>
-
-            <div class="row">
-                <div class="col-12 border-bottom border-red mb-2"><h4>Hall of Mirrors</h4></div>
-                <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/hall_of_mirrors.png"></div>
-            </div>
+            <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/asteroid_belt_2-low_res.png"></div>
         </div>
-        <?php include 'global-footer.html'; ?>
-        <script>$(document).ready(function () { $('#navbar-doodles').addClass('active rounded'); });</script>
-    </body>
+
+        <div class="row">
+            <div class="col-12 border-bottom border-red mb-2">
+                <h4>Rocket Man</h4>
+            </div>
+            <div class="col-12 text-center"><iframe class="border border-dark mw-100" src="https://www.youtube.com/embed/fWEc1RXHcfo" frameborder="0" allowfullscreen></iframe></div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 border-bottom border-red mb-2">
+                <h4>Fish Bowl</h4>
+            </div>
+            <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/fish_bowl.png"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 border-bottom border-red mb-2">
+                <h4>For The Love Of Toast</h4>
+            </div>
+            <div class="col-12 text-center"><iframe class="border border-dark mw-100" src="https://www.youtube.com/embed/cn0Y8NESzEM" frameborder="0" allowfullscreen></iframe></div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 border-bottom border-red mb-2">
+                <h4>Hall of Mirrors</h4>
+            </div>
+            <div class="col-12 text-center"><img class="border border-dark mw-100" src="media/portfolio/hall_of_mirrors.png"></div>
+        </div>
+    </div>
+    <?php include 'global-footer.html'; ?>
+    <script>
+        $(document).ready(function() {
+            $('#navbar-doodles').addClass('active rounded');
+        });
+    </script>
+</body>
+
 </html>
